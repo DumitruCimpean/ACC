478a2b9e79f8ab4f7e7842bb726549e4e90d135e
DumitruCimpean <dumitrucimpean9@gmail.com>
2024-01-24 18:16:49 +0200

    Anime Skribble done
    - Leaderboard finished today, maybe will do more styling in the future

--- app/src/main/java/com/example/acc/Board.java
+++ app/src/main/java/com/example/acc/Board.java
@@ -1,47 +1,87 @@
 package com.example.acc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
-import android.content.res.AssetManager;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import jxl.Sheet;
-import jxl.Workbook;
-import jxl.read.biff.BiffException;
-
 public class Board extends AppCompatActivity {
 
-    TextView teamList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
-        teamList = findViewById(R.id.teamLeaderboard);
+        LinearLayout rankLayout = findViewById(R.id.rankLayout);
 
         Intent intent = getIntent();
         HashMap<String, Integer> receivedRanking = (HashMap<String, Integer>) intent.getSerializableExtra("rankingMap");
         if (receivedRanking != null){
-            List<Map.Entry<String, Integer>> sortedEntries = new ArrayList<>(receivedRanking.entrySet());
-            sortedEntries.sort((entry1, entry2) -> {
+            List<Map.Entry<String, Integer>> sortedTeams = new ArrayList<>(receivedRanking.entrySet());
+            sortedTeams.sort((entry1, entry2) -> {
                 // Sorting in descending order.
                 return entry2.getValue().compareTo(entry1.getValue());
             });
-            for (Map.Entry<String, Integer> entry : sortedEntries) {
+
+            int i = 0;
+            for (Map.Entry<String, Integer> entry : sortedTeams) {
                 String key = entry.getKey();
                 int value = entry.getValue();
-                teamList.setText(key + " " + value);
-                // TODO: display the final leaderboard
+                i += 1;
+
+                // Declarations
+                RelativeLayout positionLayout = new RelativeLayout(this);
+                RelativeLayout.LayoutParams positionLayoutParams = new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.MATCH_PARENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT);
+                positionLayoutParams.setMargins(30, 5, 30, 5);
+                positionLayout.setLayoutParams(positionLayoutParams);
+
+                TextView nameItem = new TextView(this);
+                TextView scoreItem = new TextView(this);
+                nameItem.setText(i + ". " + key);
+                scoreItem.setText(String.valueOf(value));
+
+                // Styling and formatting
+                nameItem.setLayoutParams(new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT));
+                nameItem.setTextColor(Color.parseColor("#FFFFFF"));
+                nameItem.setTextSize(30);
+                nameItem.setSingleLine();
+                nameItem.setEllipsize(TextUtils.TruncateAt.END);
+
+                scoreItem.setLayoutParams(new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT));
+                scoreItem.setTextColor(Color.parseColor("#FFFFFF"));
+                scoreItem.setTextSize(30);
+
+                // Params to arrange the name and score in the position layout
+                RelativeLayout.LayoutParams nameLayoutParams = (RelativeLayout.LayoutParams) nameItem.getLayoutParams();
+                nameLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_START);
+                nameLayoutParams.setMarginEnd(60);
+                nameItem.setLayoutParams(nameLayoutParams);
+                RelativeLayout.LayoutParams scoreLayoutParams = (RelativeLayout.LayoutParams) scoreItem.getLayoutParams();
+                scoreLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_END);
+                scoreItem.setLayoutParams(scoreLayoutParams);
+
+                positionLayout.addView(nameItem);
+                positionLayout.addView(scoreItem);
+                rankLayout.addView(positionLayout);
             }
         }
 
